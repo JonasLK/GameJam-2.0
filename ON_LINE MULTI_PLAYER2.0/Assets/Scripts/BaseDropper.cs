7a62f9ae4d251fca50dc8031225e7f8c05fbac4f
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2019-07-02 15:59:51 +0200

    Update
    Minecarts excist

--- ON_LINE MULTI_PLAYER2.0/Assets/Scripts/BaseDropper.cs
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/BaseDropper.cs
@@ -6,19 +6,46 @@ public abstract class BaseDropper : BaseMachine
 {
     public GameObject dropObject;
     public Transform dropPoint;
+    public float minecartLowerAmount;
+    public float lowerSpeed;
     public int dropAmount;
     public int oreCapacity;
+
+    public GameObject cart;
     // Start is called before the first frame update
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
-            StartCoroutine(StartDropping(gameObject, 0.3f));
+            StartCoroutine(StartLoading(cart, 0.3f));
         }
     }
-    public IEnumerator StartDropping(GameObject collectorToDropInto, float loadDelay)
+    public IEnumerator StartLoading(GameObject collectorToDropInto, float loadDelay)
     {
+        Vector3 ogPosition = collectorToDropInto.transform.position;
+
+        while(Vector3.Distance(collectorToDropInto.transform.position, ogPosition + new Vector3(0, minecartLowerAmount, 0)) > 0.2f)
+        {
+            collectorToDropInto.transform.position = Vector3.MoveTowards(collectorToDropInto.transform.position, ogPosition + new Vector3(0, minecartLowerAmount, 0), lowerSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        int amountToDrop = dropAmount;
+        for (int droppedAmount = 0; droppedAmount < amountToDrop; droppedAmount++)
+        {
+            dropAmount--;
+            yield return new WaitForSeconds(loadDelay);
+        }
+
+        //LOADING SHIT IN
+        while (Vector3.Distance(collectorToDropInto.transform.position, ogPosition) != 0)
+        {
+            collectorToDropInto.transform.position = Vector3.MoveTowards(collectorToDropInto.transform.position, ogPosition, lowerSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+        StartCoroutine(collectorToDropInto.GetComponent<Minecart>().MoveToOrigin());
+        /*
         int amountToDrop = dropAmount;
         for(int droppedAmount = 0; droppedAmount < amountToDrop; droppedAmount++)
         {
@@ -26,7 +53,7 @@ public abstract class BaseDropper : BaseMachine
             Instantiate(dropObject, dropPoint.transform.position, Quaternion.identity);
         }
         dropAmount -= amountToDrop;
-        //Tell collector to proceed
+        //Tell collector to proceed*/
     }
     public abstract IEnumerator GenerateDrops();
 }
---
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/Minecart.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minecart : MonoBehaviour
+{
+    public Transform startPoint;
+    public BaseDropper connectedMine;
+    public float moveSpeed;
+    public float loadSpeed;
+    public float unloadSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveToTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public IEnumerator MoveToTarget()
+    {
+        while(Vector3.Distance(transform.position, connectedMine.dropPoint.position) > 0.2f)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, connectedMine.dropPoint.position, moveSpeed * Time.deltaTime);
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+            yield return null;
+        }
+        StartCoroutine(connectedMine.StartLoading(gameObject, loadSpeed));
+    }
+    public IEnumerator MoveToOrigin()
+    {
+        while (Vector3.Distance(transform.position, startPoint.position) > 0.2f)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, startPoint.position, moveSpeed * Time.deltaTime);
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+            yield return null;
+        }
+        StartCoroutine(MoveToTarget());
+    }
+}
