5c0f23d84cd7070061021407c9254a2732dc6efa
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2019-07-03 15:48:24 +0200

    Player Implementation

--- ON_LINE MULTI_PLAYER2.0/Assets/Scripts/Movement.cs
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/Movement.cs
@@ -6,18 +6,21 @@ public class Movement : MonoBehaviour
 {
     public float hor, ver, cHor, cVer;
     public float movespeed;
-    public float rotateSpeed;
+    public float rotateSpeed, bodyRotateSpeed;
     public float walkingSpeed, runningSpeed;
     public Vector3 move, camMoveX, camMoveY;
     public float testx, testxmax;
     public float maxZoom, minZoom;
-    public bool walking, running;
+    public bool walking, running, moving;
     public GameObject cam;
     public GameObject actualCam;
+    public Transform actualBodyModel;
+    public Vector3 walkDirection = new Vector3();
 
     public void Awake()
     {
         movespeed = walkingSpeed;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -31,10 +34,10 @@ public class Movement : MonoBehaviour
         cVer = Input.GetAxis("Mouse Y");
         camMoveY.y = cHor;
         camMoveX.x = -cVer;
-        Vector3 walkDirection = new Vector3();
+        walkDirection = Vector3.zero;
         if(hor != 0|| ver != 0)
         {
-            walking = true;
+            
             if(hor != 0)
             {
                 if(hor > 0)
@@ -45,6 +48,7 @@ public class Movement : MonoBehaviour
                 {
                     walkDirection -= cam.transform.right * -hor;
                 }
+                walking = true;
             }
             if(ver != 0)
             {
@@ -56,9 +60,16 @@ public class Movement : MonoBehaviour
                 {
                     walkDirection -= cam.transform.forward * -ver;
                 }
+                walking = true;
             }
         }
-        if (Input.GetButton("Fire3"))
+        else
+        {
+            walking = false;
+            running = false;
+            moving = false;
+        }
+        if (Input.GetButton("Run"))
         {
             running = true;
             walking = false;
@@ -66,9 +77,12 @@ public class Movement : MonoBehaviour
         }
         else
         {
-            walking = true;
             running = false;
             movespeed = walkingSpeed;
+        }
+        if(walking || running)
+        {
+            moving = true;
         }
         walkDirection.y = 0;
         move = walkDirection;
@@ -98,8 +112,11 @@ public class Movement : MonoBehaviour
             newEuler.x = tempClamp;
             cam.transform.eulerAngles = newEuler;
             cam.transform.Rotate(camMoveX * Time.deltaTime * rotateSpeed, Space.Self);
+            //gameObject.transform.Rotate(camMoveY * Time.deltaTime * rotateSpeed);
             cam.transform.Rotate(camMoveY * Time.deltaTime * rotateSpeed, Space.World); //y world, x local
         }
+        Quaternion rotationWeWnat = Quaternion.LookRotation(walkDirection);
+        actualBodyModel.transform.rotation = Quaternion.RotateTowards(actualBodyModel.transform.rotation, rotationWeWnat, bodyRotateSpeed);
         transform.Translate(move * Time.deltaTime * movespeed);
     }
 }
---
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/TestAnime.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnime : MonoBehaviour
+{
+    public Animator ani;
+    public Movement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponentInChildren<Animator>();
+        player = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.moving)
+        {
+            if(player.walking)
+            {
+                AnimationReset();
+                ani.SetTrigger("Walk");
+            }
+            if (player.running)
+            {
+                AnimationReset();
+                ani.SetTrigger("Run");
+            }
+        }
+        else
+        {
+            AnimationReset();
+            ani.SetTrigger("Idle");
+        }
+    }
+    public void AnimationReset()
+    {
+        ani.ResetTrigger("Walk");
+        ani.ResetTrigger("Run");
+        ani.ResetTrigger("Idle");
+    }
+}
