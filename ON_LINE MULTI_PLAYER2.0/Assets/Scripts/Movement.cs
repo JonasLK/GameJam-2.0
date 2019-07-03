5c4a9e5181a04a48ea51df2a29b8b0a1f0972d80
LAPTOP-C8F0QODP\Joël <Jsmjohn@hotmail.com>
2019-07-03 11:39:10 +0200

    Running + Zoom

--- ON_LINE MULTI_PLAYER2.0/Assets/Scripts/Movement.cs
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/Movement.cs
@@ -7,13 +7,17 @@ public class Movement : MonoBehaviour
     public float hor, ver, cHor, cVer;
     public float movespeed;
     public float rotateSpeed;
+    public float walkingSpeed, runningSpeed;
     public Vector3 move, camMoveX, camMoveY;
     public float testx, testxmax;
+    public float maxZoom, minZoom;
+    public bool walking, running;
     public GameObject cam;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject actualCam;
 
+    public void Awake()
+    {
+        movespeed = walkingSpeed;
     }
 
     // Update is called once per frame
@@ -27,25 +31,10 @@ public class Movement : MonoBehaviour
         cVer = Input.GetAxis("Mouse Y");
         camMoveY.y = cHor;
         camMoveX.x = -cVer;
-    }
-    public void FixedUpdate()
-    {
-        
-        if (Input.GetButton("Fire2"))
+        Vector3 walkDirection = new Vector3();
+        if(hor != 0|| ver != 0)
         {
-            Vector3 newEuler = cam.transform.localEulerAngles;
-            float tempClamp = newEuler.x;
-            print(newEuler);
-            if (newEuler.x > 180)
-            {
-                tempClamp -= 360;
-            }
-            tempClamp = Mathf.Clamp(tempClamp, testx, testxmax);
-            newEuler.x = tempClamp;
-            cam.transform.eulerAngles = newEuler;
-            cam.transform.Rotate(camMoveX * Time.deltaTime * rotateSpeed, Space.Self);
-            cam.transform.Rotate(camMoveY * Time.deltaTime * rotateSpeed, Space.World); //y world, x local
-            Vector3 walkDirection = new Vector3();
+            walking = true;
             if(hor != 0)
             {
                 if(hor > 0)
@@ -68,8 +57,48 @@ public class Movement : MonoBehaviour
                     walkDirection -= cam.transform.forward * -ver;
                 }
             }
-            walkDirection.y = 0;
-            move = walkDirection;
+        }
+        if (Input.GetButton("Fire3"))
+        {
+            running = true;
+            walking = false;
+            movespeed = runningSpeed;
+        }
+        else
+        {
+            walking = true;
+            running = false;
+            movespeed = walkingSpeed;
+        }
+        walkDirection.y = 0;
+        move = walkDirection;
+        float cameraDis = Vector3.Distance(actualCam.transform.position, cam.transform.position);
+        if(Input.GetAxis("Mouse ScrollWheel") > 0 && cameraDis >= minZoom)
+        {
+            actualCam.transform.position += actualCam.transform.forward;
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0 && cameraDis <= maxZoom)
+        {
+            actualCam.transform.position -= actualCam.transform.forward;
+        }
+    }
+    public void FixedUpdate()
+    {
+        
+        if (Input.GetButton("Fire2"))
+        {
+            Vector3 newEuler = cam.transform.localEulerAngles;
+            float tempClamp = newEuler.x;
+            print(newEuler);
+            if (newEuler.x > 180)
+            {
+                tempClamp -= 360;
+            }
+            tempClamp = Mathf.Clamp(tempClamp, testx, testxmax);
+            newEuler.x = tempClamp;
+            cam.transform.eulerAngles = newEuler;
+            cam.transform.Rotate(camMoveX * Time.deltaTime * rotateSpeed, Space.Self);
+            cam.transform.Rotate(camMoveY * Time.deltaTime * rotateSpeed, Space.World); //y world, x local
         }
         transform.Translate(move * Time.deltaTime * movespeed);
     }
