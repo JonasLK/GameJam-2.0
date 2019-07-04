cd18f0a5b83285bae793b4f2b332b4435f2fae10
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2019-07-04 15:37:35 +0200

    I still see shadows
    from ze carrots

--- ON_LINE MULTI_PLAYER2.0/Assets/Scripts/House.cs
+++ ON_LINE MULTI_PLAYER2.0/Assets/Scripts/House.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 public class House : Building
 {
     public float populationCapacityModifier;
+    public float capacity;
+    public float currentAmount;
+    public float generationDelay;
+    public float foodConsumption;
     void Start()
     {
         GameObject.FindGameObjectWithTag("Manager").GetComponent<LocalGameManager>().populationCapacity += populationCapacityModifier;
+        StartCoroutine(StartPopgeneration());
+    }
+
+    public IEnumerator StartPopgeneration()
+    {
+        LocalGameManager manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<LocalGameManager>();
+        while (currentAmount < capacity)
+        {
+            yield return new WaitForSeconds(generationDelay);
+            if(currentAmount < capacity)
+            {
+                if(manager.food >= foodConsumption)
+                {
+                    currentAmount += 1;
+                    manager.population += 1;
+                    manager.food -= foodConsumption;
+                    manager.RecalculatePopulationBoost();
+                }
+            }
+        }
     }
 }
